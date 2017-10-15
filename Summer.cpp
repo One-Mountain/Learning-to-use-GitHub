91a052f7470aa9e1cc466c1a4515baabd377f454
One-Mountain <juan.montano01@gmail.com>
2017-10-15 15:49:14 -0700

    Add files via upload

---
+++ Summer.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	int a;
+	int b;
+	
+	cout << "Please input the first integer:"<< endl;
+	cin >> a;
+	cout << "Please input the second integer:"<< endl;
+	cin >> b;
+	
+	cout <<"The sum of "<< a << " and "<< b <<" is:" << endl;
+	cout << a+b << endl;
+	
+	return 0;
+}
